a57ece36fbad01c38008d1d19ba87e7794f24fa5
CaneRig <originalomail@gmail.com>
2025-07-05 05:13:31 +0300

    Added onChanged callback to the registration fields

--- lib/pages/patient_registration/patient_registartion_view.dart
+++ lib/pages/patient_registration/patient_registartion_view.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
-class PatientRegistrationViewState{
+class PatientRegistrationViewState {
   late final PatientRegistrationModel _model;
 
   PatientRegistrationViewState(this._model);
@@ -15,72 +15,85 @@ class PatientRegistrationViewState{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Страница регестрации пациента')),
-      body: Center(child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          color: const Color.fromARGB(255, 194, 199, 191),
-        ),
-        padding: EdgeInsets.all(10),
-        child: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsetsGeometry.all(10),
-          child: Shortcuts(
-            shortcuts: const <ShortcutActivator, Intent>{
-              //При нажатии на "Enter" переходим к следующему полю ввода
-              SingleActivator(LogicalKeyboardKey.enter): NextFocusIntent(),
-            },
-              child: Column(
-                spacing: 20,
-              crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text("Данные пациента:", 
-              style: const TextStyle(
-                fontSize: 23.0,
-                height: 1, 
-                color: Color.fromARGB(255, 0, 0, 0),
-                fontWeight: FontWeight.bold,
-                )
-              ),
-
-              Form(
-                autovalidateMode: AutovalidateMode.always,
-                onChanged: (){
-                  Form.of(primaryFocus!.context!).save();
-                }, 
-                child: Column(
-                  spacing: 10,
-                children: [
-                  CustomTextFormField(_model, "Введите id", Icons.assignment,  FilteringTextInputFormatter.digitsOnly),
-                  CustomTextFormField(_model, "Введите имя", Icons.person_rounded,  FilteringTextInputFormatter.deny('/t')),
-                  CustomTextFormField(_model, "Введите фамилию", Icons.person_rounded, FilteringTextInputFormatter.deny('/t')),
-                  CustomTimeFormField(_model, "Время приёма")
-                ]
-              )
-              ),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
-                  ),
-                onPressed: () {//переход на страницу плеера
-              print("Hi");
-              },
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: const Color.fromARGB(255, 194, 199, 191),
+          ),
+          padding: EdgeInsets.all(10),
+          child: SingleChildScrollView(
               child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                child: Text("Далее", style: const TextStyle(
-          fontSize: 20.0,
-          height: 1,
-          color: Color.fromARGB(255, 255, 255, 255),
-          ),
+                  padding: EdgeInsetsGeometry.all(10),
+                  child: Shortcuts(
+                    shortcuts: const <ShortcutActivator, Intent>{
+                      //При нажатии на "Enter" переходим к следующему полю ввода
+                      SingleActivator(LogicalKeyboardKey.enter):
+                          NextFocusIntent(),
+                    },
+                    child: Column(
+                      spacing: 20,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Данные пациента:",
+                            style: const TextStyle(
+                              fontSize: 23.0,
+                              height: 1,
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              fontWeight: FontWeight.bold,
+                            )),
+                        Form(
+                            autovalidateMode: AutovalidateMode.always,
+                            onChanged: () {
+                              Form.of(primaryFocus!.context!).save();
+                            },
+                            child: Column(spacing: 10, children: [
+                              CustomTextFormField(
+                                  _model,
+                                  "Введите id",
+                                  Icons.assignment,
+                                  FilteringTextInputFormatter.digitsOnly,
+                                  _model.setId),
+                              CustomTextFormField(
+                                  _model,
+                                  "Введите имя",
+                                  Icons.person_rounded,
+                                  FilteringTextInputFormatter.deny('/t'),
+                                  _model.setName),
+                              CustomTextFormField(
+                                  _model,
+                                  "Введите фамилию",
+                                  Icons.person_rounded,
+                                  FilteringTextInputFormatter.deny('/t'),
+                                  _model.setSurname),
+                              CustomTimeFormField(
+                                  _model, "Время приёма", _model.setTime)
+                            ])),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green,
+                          ),
+                          onPressed: () {
+                            //переход на страницу плеера
+                            print("Hi");
+                          },
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 10),
+                            child: Text(
+                              "Далее",
+                              style: const TextStyle(
+                                fontSize: 20.0,
+                                height: 1,
+                                color: Color.fromARGB(255, 255, 255, 255),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ))),
         ),
-        ),
-        ),
-            ],
-          ),
-          )
-      )
-    ),
-    ),
-    ),
+      ),
     );
   }
 }
--- lib/pages/patient_registration/patient_registration_model.dart
+++ lib/pages/patient_registration/patient_registration_model.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class PatientRegistrationModel{
+class PatientRegistrationModel {
   late String _name;
   late String _surname;
   late int _id;
   late DateTime _time;
+
+  void setName(String newValue) => _name = newValue;
+  void setSurname(String newValue) => _surname = newValue;
+  void setId(String newValue) => _id = int.tryParse(newValue) ?? -1;
+  void setTime(DateTime newValue) => _time = newValue;
 }
